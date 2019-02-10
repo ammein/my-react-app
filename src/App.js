ad6288dad249009d72400f2c3f1d8a6aa2ddfa3c
Amin Shazrin <aminshazrin@yahoo.com>
2019-02-10 15:52:32 +0800

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    // return (
+    //   <div className="App">
+    //     <h1>Hi , I am react App</h1>
+    //     <p>Test Web App</p>
+    //   </div>
+    // );
+    return React.createElement('div' , {className : 'App'} , React.createElement('h1' , null , 'Does this work now ?'));
+  }
+}
+
+export default App;
