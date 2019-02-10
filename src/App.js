7d357ba5d050733cc6221db053d5d9add732cc3d
Amin Shazrin <aminshazrin@yahoo.com>
2019-02-11 00:50:37 +0800

    Later Sambung lagi. React is cool !

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person.js';
 
 class App extends Component {
   render() {
-    // return (
-    //   <div className="App">
-    //     <h1>Hi , I am react App</h1>
-    //     <p>Test Web App</p>
-    //   </div>
-    // );
-    return React.createElement('div' , {className : 'App'} , React.createElement('h1' , null , 'Does this work now ?'));
+    return (
+      <div className="App">
+        <h1>Hi , I am react App</h1>
+        <p>Test Web App</p>
+        <p>This is working too</p>
+        <Person /> {/* You could also use <Person></Person> */}
+      </div>
+    );
   }
 }
 
